b2f03bc0f564167bd896a5034c4a1404cd03319a
PintoJR <guimoraespinto@gmail.com>
2025-02-14 16:56:27 -0400

    versao

---
+++ src/exercicio26.ts
@@ -0,0 +1,19 @@
+function multiplicacao (fator1 : number, fator2 : number ): number
+{
+    return fator1*fator2
+} 
+
+multiplicacao(5,10)
+
+console.log('Resultado da multiplicaçao : ')
+console.log(multiplicacao)
+
+function olaNome(nome1:string) :string
+{
+    return nome1
+}
+
+olaNome('Guilherme')
+
+console.log('Ola ')
+console.log(olaNome)
