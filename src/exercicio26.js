9492e0a1cebfa1ba81fd413da78935aebd06be13
PintoJR <guimoraespinto@gmail.com>
2025-02-21 15:22:49 -0400

    versão2

---
+++ src/exercicio26.js
@@ -0,0 +1,14 @@
+"use strict";
+// --- MODULO 26  ---- //
+// Multiplicacao
+function multiplicacao(fator1, fator2) {
+    return fator1 * fator2;
+}
+console.log('Resultado da multiplicaçao : ');
+console.log(multiplicacao(5, 10));
+// Ola Nome
+function olaNome(nome1) {
+    return nome1;
+}
+console.log('Ola ' + (olaNome('Guilherme')));
+console.log(olaNome('Guilherme'));
--- src/exercicio26.ts
+++ src/exercicio26.ts
@@ -6,10 +6,8 @@ function multiplicacao (fator1 : number, fator2 : number ): number
     return fator1*fator2
 } 
 
-multiplicacao(5,10)
-
 console.log('Resultado da multiplicaçao : ')
-console.log(multiplicacao)
+console.log(multiplicacao(5,10))
 
 // Ola Nome
 function olaNome(nome1:string) :string
@@ -17,7 +15,6 @@ function olaNome(nome1:string) :string
     return nome1
 }
 
-olaNome('Guilherme')
+console.log('Ola '+ (olaNome('Guilherme')))
 
-console.log('Ola ')
-console.log(olaNome)
+
